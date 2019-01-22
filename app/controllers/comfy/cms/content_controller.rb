65e8bbf74ab04579cd264c72ed5b9d98061d157d
Marcin Skłodowski <marcinsklodowski@gmail.com>
2019-01-22 09:08:23 +0100

    content controller update

--- app/controllers/comfy/cms/content_controller.rb
+++ app/controllers/comfy/cms/content_controller.rb
@@ -1,95 +1,92 @@
+# frozen_string_literal: true
+
 class Comfy::Cms::ContentController < Comfy::Cms::BaseController
 
   # Authentication module must have `authenticate` method
   include ComfortableMexicanSofa.config.public_auth.to_s.constantize
 
+  # Authorization module must have `authorize` method
+  include ComfortableMexicanSofa.config.public_authorization.to_s.constantize
+
   before_action :load_seeds
   before_action :load_cms_page,
-                :authenticate,
-                :only => :show
-
-  rescue_from ActiveRecord::RecordNotFound, :with => :page_not_found
+    :authenticate,
+    :authorize,
+    only: :show
 
   def show
-
     if @cms_page.target_page.present?
-      redirect_to @cms_page.target_page.url(:relative)
+      redirect_to @cms_page.target_page.url(relative: true)
     else
       respond_to do |format|
         format.html { render_page }
-        format.json { render :json => @cms_page }
+        format.json do
+          json_page = @cms_page.as_json(except: [:content_cache])
+          json_page[:content] = render_to_string(
+            inline: @cms_page.content_cache,
+            layout: false
+          )
+          render json: json_page
+        end
       end
     end
   end
 
-  def render_sitemap
-    render
-  end
+  protected
 
-protected
-
-  def render_page(status = 200)
-    if @cms_layout = @cms_page.layout
-      app_layout = (@cms_layout.app_layout.blank? || request.xhr?) ? false : @cms_layout.app_layout
-      render  :inline       => @cms_page.content_cache,
-              :layout       => app_layout,
-              :status       => status,
-              :content_type => mime_type
-    else
-      render :text => I18n.t('comfy.cms.content.layout_not_found'), :status => 404
-    end
+  def render_page(status = :ok)
+    render  inline:       @cms_page.content_cache,
+      layout:       app_layout,
+      status:       status,
+      content_type: mime_type
   end
 
   # it's possible to control mimetype of a page by creating a `mime_type` field
   def mime_type
-    mime_block = @cms_page.blocks.find_by_identifier(:mime_type)
-    mime_block && mime_block.content || 'text/html'
+    mime_block = @cms_page.fragments.detect { |f| f.identifier == "mime_type" }
+    mime_block&.content || "text/html"
+  end
+
+  def app_layout
+    return false if request.xhr? || !@cms_layout
+    @cms_layout.app_layout.present? ? @cms_layout.app_layout : false
   end
 
   def load_seeds
     return unless ComfortableMexicanSofa.config.enable_seeds
-
-    controllers = %w[layouts pages snippets files].collect { |c| "comfy/admin/cms/" + c }
-    if controllers.member?(params[:controller]) && params[:action] == "index"
-      ComfortableMexicanSofa::Seeds::Importer.new(@site.identifier).import!
-      flash.now[:warning] = I18n.t("comfy.admin.cms.base.seeds_enabled")
-    end
+    ComfortableMexicanSofa::Seeds::Importer.new(@cms_site.identifier).import!
   end
 
-
+  # Attempting to populate @cms_page and @cms_layout instance variables so they
+  # can be used in view helpers/partials
   def load_cms_page
-
-    page = @cms_site.pages.published.find_by_full_path("/#{params[:cms_path]}")
-
-    if page.present?
-      @cms_page = page
-    else
-
-      old_page = Comfy::Cms::Block.where(identifier: 'seo.old_page_url').where('content LIKE ?', "%#{params[:cms_path].to_s}%").pluck(:content, :blockable_id, :blockable_type).first
-
-      if old_page.present?
-        if old_page.last == "Comfy::Cms::Page"
-          new_page = Comfy::Cms::Page.find_by(id: old_page.second)
-
-          if new_page.present?
-            redirect_to new_page.url(:relative), status: 301
-
-            return
-          end
-        end
+    unless find_cms_page_by_full_path("/#{params[:cms_path]}")
+      if find_cms_page_by_full_path("/404")
+        render_page(:not_found)
+      else
+        message = "Page Not Found at: \"#{params[:cms_path]}\""
+        raise ActionController::RoutingError, message
       end
-
-      page_not_found
     end
   end
 
-  def page_not_found
-    @cms_page = @cms_site.pages.published.find_by_full_path!('/404')
+  # Getting page and setting content_cache and fragments data if we need to
+  # serve translation data
+  def find_cms_page_by_full_path(full_path)
+    @cms_page   = @cms_site.pages.published.find_by!(full_path: full_path)
+    @cms_layout = @cms_page.layout
 
-    respond_to do |format|
-      format.html { render_page(404) }
+    # There are translations for this page and locale is not the default site
+    # locale, so we need to grab translation data.
+    if @cms_page.translations.any? && @cms_site.locale != I18n.locale.to_s
+      @cms_page.translate!(I18n.locale)
+      @cms_layout = @cms_page.layout
     end
+
+    @cms_page
+
   rescue ActiveRecord::RecordNotFound
-    raise ActionController::RoutingError.new("Page Not Found at: \"#{params[:cms_path]}\"")
+    nil
   end
+
 end
